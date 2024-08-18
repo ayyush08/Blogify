97b32da3d4fb3ce5c5943a157cd82a74d1bd2b81
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2024-08-18 16:47:47 +0530

    chore: Add blogs model to backend

---
+++ backend/src/models/blogs.model.js
@@ -0,0 +1,22 @@
+import mongoose, { Schema } from "mongoose";
+
+
+const blogsSchema = new Schema({
+    owner:{
+        type:Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    title:{
+        type:String,
+        required:true,
+    },
+    content:{
+        type:String,
+        required:true,
+    }
+},{
+    timestamps:true
+}) 
+
+export const Blogs = mongoose.model('Blogs',blogsSchema)
