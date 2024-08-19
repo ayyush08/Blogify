91dae4cbce34a05a6f3a1367eda3ba401d0f28aa
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2024-08-19 15:25:45 +0530

    feat: Add API endpoint for uploading blog posts

---
+++ backend/src/controllers/blogs.controller.js
@@ -0,0 +1,32 @@
+import { Blogs } from "../models/blogs.model";
+import { User } from "../models/user.model";
+import { asyncHandler } from "../utils/asyncHandler";
+import { ApiResponse } from "../utils/ApiResponse";
+import { ApiError } from "../utils/ApiError";
+import { isValidObjectId } from "mongoose";
+
+
+const uploadBlog = asyncHandler(async(req,res)=>{
+    const {userId} = req.params;
+    const {title,content} = req.body;
+
+    if(!isValidObjectId(userId)){
+        throw new ApiError(400,'Invalid user id')
+    }
+
+    if(!title || !content){
+        throw new ApiError(400,'Please fill all fields')
+    }
+    const user = await User.findById(userId);
+    if(!user){
+        throw new ApiError(404,'User not found')
+    }
+    const blogpost = await Blog.create({
+        owner:userId,
+        title,
+        content
+    })
+    return res
+    .status(200)
+    .json(new ApiResponse(200,blogpost,'Blog created successfully'))
+})
