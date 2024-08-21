2f042a345129258aae1d7aa4e751bd6c39bb8e5e
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2024-08-21 22:39:43 +0530

    feat: Add API endpoint for updating blog content

--- backend/src/routes/blogs.route.js
+++ backend/src/routes/blogs.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { uploadBlog } from "../controllers/blogs.controller";
+import { uploadBlog,updateBlogContent } from "../controllers/blogs.controller";
 
 
 const router = Router();
 
 router.route('/upload/:userId').post(uploadBlog);
+router.route('/update/:blogId').put(updateBlogContent);
 
 
 
